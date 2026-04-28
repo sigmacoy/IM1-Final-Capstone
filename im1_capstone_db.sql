057a35bb7e32f934844ea64dacd4cd10d4f66593
macooyb <macoybacalla9@gmail.com>
2026-04-28 14:10:32 +0800

    fixed sql withh the create and pressed structure sa export

--- im1_capstone_db.sql
+++ im1_capstone_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 28, 2026 at 08:01 AM
+-- Generation Time: Apr 28, 2026 at 08:09 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -21,6 +21,20 @@ SET time_zone = "+00:00";
 -- Database: `im1_capstone_db`
 --
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `User`
+--
+
+CREATE TABLE `User` (
+  `user_id` int(11) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 --
 -- Dumping data for table `User`
 --
@@ -28,6 +42,27 @@ SET time_zone = "+00:00";
 INSERT INTO `User` (`user_id`, `first_name`, `last_name`, `email`, `password`) VALUES
 (1, 'Admin', 'User', 'admin@cit.edu', '$2y$12$xAou2y20gZZ.li94Fp.z6eM47Oq7Zsx0d33XUKgTzsCJcXDWOQo7.'),
 (2, 'Admin', 'User', 'admin', '$2y$12$3NyPzkqkhrv8r7rMdv89betGz32dCe6MQGn1ZvbxYl3JBMpt7DLSi');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `User`
+--
+ALTER TABLE `User`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `User`
+--
+ALTER TABLE `User`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
